bf49d8c4ee79c1beb857f39fc8b7273c731773a3
NatanGoes777 <natan.goes777@gmail.com>
2022-08-11 17:14:31 -0300

    end point findAll para user

--- src/main/java/br/com/natan/api/services/UserService.java
+++ src/main/java/br/com/natan/api/services/UserService.java
@@ -2,8 +2,11 @@ package br.com.natan.api.services;
 
 import br.com.natan.api.domain.User;
 
+import java.util.List;
+
 public interface UserService {
 
     User findById(Integer id);
+    List<User> findAll();
 
 }
